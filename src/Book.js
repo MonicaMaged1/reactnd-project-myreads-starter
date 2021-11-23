ecdd46bc43261d18c348773ba56f8abc215a3f11
MonicaLabib <mlabib@ejada.com>
2021-11-23 14:55:17 +0200

    destructured the project into components

---
+++ src/Book.js
@@ -0,0 +1,43 @@
+import React from 'react'
+
+const Book = props => {
+    const { book } = props
+    let authors;
+
+    try {
+        authors = book.authors.join(",")
+    }
+    catch (e) {
+        authors = ""
+    }
+    let imageUrl;
+
+    try {
+        imageUrl = book.imageLinks.thumbnail
+    }
+    catch (e) {
+        imageUrl = ""
+    }
+
+    return (
+        <div className="book">
+            <div className="book-top">
+                <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: 'url(' + imageUrl + ')' }}></div>
+                <div className="book-shelf-changer">
+                    <select defaultValue={book.shelf ? book.shelf : "none"}>
+                        <option value="move" disabled>Move to...</option>
+                        <option value="currentlyReading">Currently Reading</option>
+                        <option value="wantToRead">Want to Read</option>
+                        <option value="read">Read</option>
+                        <option value="none">None</option>
+                    </select>
+                </div>
+            </div>
+            <div className="book-title">{book.title}</div>
+            <div className="book-authors">{authors}</div>
+        </div>
+
+    )
+}
+
+export default Book
---
+++ src/ListBooks.js
@@ -0,0 +1,37 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+import Shelf from './Shelf'
+import PropTypes from 'prop-types';
+
+const ListBooks = props => {
+    const {books} = props
+    const shelves = {
+        currentlyReading: "Currently Reading",
+        read: "Read",
+        wantToRead: "Want To Read"
+    }
+
+    return (
+        <div className="list-books">
+            <div className="list-books-title">
+                <h1>MyReads</h1>
+            </div>
+            <div className="list-books-content">
+                <div>
+                    {Object.keys(shelves).map((shelf => (<Shelf key={shelf} title={shelves[shelf]} books={books.filter((b) => b.shelf === shelf)} />)))}
+                </div>
+            </div>
+            <div className="open-search">
+                <Link to="/search">Add a book</Link>
+            </div>
+        </div>
+    )
+
+
+}
+
+ListBooks.propTypes = {
+    books : PropTypes.array
+}
+
+export default ListBooks
